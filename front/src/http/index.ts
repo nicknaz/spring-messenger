b20d38590dafad469a1986c71c957c157a71a235
nicknaz <kolya.nazarov41@gmail.com>
2024-03-21 14:33:17 +0300

    test: github-actions

--- front/src/http/index.ts
+++ front/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AuthResponse } from "../models/AuthReaponse";
 
 export const SERVER_URL = `http://localhost:5600/messenger`;
 
@@ -14,5 +15,22 @@ $api.interceptors.request.use(
         return config;
     }
 )
+
+$api.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${SERVER_URL}/auth/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.token);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('НЕ АВТОРИЗОВАН')
+        }
+    }
+    throw error;
+})
 
 export default $api;
